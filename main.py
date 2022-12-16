78eec31e674bf974f754d5e03beb900c2fd57a02
Thibauld Croonenborghs <croonenborghsthibauld@gmail.com>
2022-12-16 20:13:41 +0100

    feat: add file removal

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from tkinter.simpledialog import askstring
 from typing import List
 
 
-# TODO: add combobox -> add remove button -> add VS code open button -> button for sorting on file/dir name or timestamp created
-# TODO: nice to have: make entry/combobox flash red if path does not exist
+# TODO: add remove button -> add VS code open button -> button for sorting on file/dir name or timestamp created
+# TODO: nice to have: make combobox flash red if path does not exist
 
 def populate_combo(path: str) -> List[str]:
     all_dirs_up = []
@@ -42,11 +42,26 @@ def rename_file() -> None:
     if list_box == msgbox:
        files, path = l_files, first_path_combo
     elif list_box == msgbox2:
-       files, path = r_files, second_path
+       files, path = r_files, second_path_combo
     old_path = path.get() + "/" + list_box.get(list_box.curselection()[0])[2:]
     new_name = askstring("Rename", "New file/directory name?")
     new_path = path.get() + "/" + new_name
     os.rename(old_path, new_path)
+    files.set(get_directory_with_icons(path))
+
+
+def remove_file() -> None:
+    files, path = tk.StringVar(), tk.StringVar()
+    list_box = [box for box in [msgbox, msgbox2] if len(box.curselection()) > 0][0]
+    if list_box == msgbox:
+       files, path = l_files, first_path_combo
+    elif list_box == msgbox2:
+       files, path = r_files, second_path_combo
+    path_to_remove = path.get() + "/" + list_box.get(list_box.curselection()[0])[2:]
+    if os.path.isdir(path_to_remove):
+        os.rmdir(path_to_remove)
+    else:
+        os.remove(path_to_remove)
     files.set(get_directory_with_icons(path))
 
 
@@ -85,7 +100,7 @@ def move_file(direction: str) -> None:
     source_path = source_folder + '/' + list_box.get(selection[0])[2:]
     shutil.move(source_path, destination_folder)
     l_files.set(get_directory_with_icons(first_path_combo))
-    r_files.set(get_directory_with_icons(second_path))
+    r_files.set(get_directory_with_icons(second_path_combo))
 
 
 if __name__ == "__main__":
@@ -110,7 +125,7 @@ if __name__ == "__main__":
     rename_button.grid(row=0, column=0, padx=5, pady=5)
     move_backward_button = tk.ttk.Button(mainframe, text="<-", command=lambda: move_file("backward"), width=2)
     move_backward_button.grid(row=0, column=1, padx=5, pady=5)
-    remove_button = tk.ttk.Button(mainframe, text="X", command=lambda: print("remove"), width=2)
+    remove_button = tk.ttk.Button(mainframe, text="X", command=lambda: remove_file(), width=2)
     remove_button.grid(row=0, column=2, padx=5, pady=5)
     move_forward_button = tk.ttk.Button(mainframe, text="->", command=lambda: move_file("forward"), width=2)
     move_forward_button.grid(row=0, column=3, padx=5, pady=5)
